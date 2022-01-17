46f9c47b93a6a26f62d84f03bfd88194b06abd06
Ryan Gauthier <ryan.eugene.gauthier@gmail.com>
2022-01-17 13:00:10 -0800

    Add files via upload

---
+++ JavaPortfolio/src/com/gauthierdevlopment/javaportfolio/MultiplesOfThreeOrFive.java
@@ -0,0 +1,8 @@
+package com.gauthierdevlopment.javaportfolio;
+
+public class MultiplesOfThreeOrFive {
+    public static void main(String[] args) {
+        String name = "Ryan";
+        System.out.println("Hello " + name + ", welcome!");
+    }
+}
